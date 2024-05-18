7a4cbb8052f30ef22300ca647e23618a335be746
samjwu <22262939+samjwu@users.noreply.github.com>
2024-05-18 11:02:20 -0600

    Specify projects to install

--- Step12/MultiCPackConfig.cmake
+++ Step12/MultiCPackConfig.cmake
@@ -1,2 +1,8 @@
 # include default config file created by CMake
 include("release/CPackConfig.cmake")
+
+# specify projects to install
+set(CPACK_INSTALL_CMAKE_PROJECTS
+    "debug;Tutorial;ALL;/"
+    "release;Tutorial;ALL;/"
+    )
